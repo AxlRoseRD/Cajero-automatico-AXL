22452f87458ea45ab8faa2e17cf0a8f5648ff873
Axcell Espinal <61571507+AxlRoseRD@users.noreply.github.com>
2021-02-09 18:28:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Cajero_automatico_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cajero Palmer = new Cajero();
+            Boolean sesion = true;
+            //valor en unidades de 1$RD de los billetes
+            /*este campo sirve para reflejar mejor cuantas unidades de 1$ se lleva el usuario 
+            en base a la cantidad de billetes enteros dispensados*/
+            Palmer.Contenedor1000 *= 1000;
+            Palmer.Contenedor500 *= 500;
+            Palmer.Contenedor200 *= 200;
+            Palmer.Contenedor100 *= 100;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("**************************************************************");
+                Console.WriteLine("*<<<<<<<<< Bienvenid@ a la red de cajeros palmer >>>>>>>>>>>>*");
+                Console.WriteLine("*------------------------------------------------------------*");
+                Console.WriteLine("*|> 1.Transaccion con otro valor       2.transaccion rapida |*");
+                Console.WriteLine("*|> 3.balance del cajero               4.salir              |*");
+                Console.WriteLine("*|                                                          |*");
+                Console.WriteLine("*|                                                          |*");
+                Console.WriteLine("*|                                                          |*");
+                Console.WriteLine("*|                                                          |*");
+                Console.WriteLine("**************************************************************");
+                Console.ForegroundColor = ConsoleColor.White;
+                int opcion = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                int transaccion;
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("digite el monto de su transaccion");
+                        transaccion = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        int residuo1 = transaccion % 100;
+                        int validez = residuo1;
+                        if (validez == 0 && transaccion >= 100)
+                        {
+                            Palmer.Pagar(transaccion);                    
+                        }
+                        else 
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ingrese un monto valido");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("******************************************");
+                        Console.WriteLine("*<<<<<<<<< Elija una transaccion >>>>>>>>*");
+                        Console.WriteLine("*----------------------------------------*");
+                        Console.WriteLine("*|> 1. 100$RD               2. 500$RD   |*");
+                        Console.WriteLine("*|> 3. 200$RD               4. 1,000$RD |*");
+                        Console.WriteLine("*|                                      |*");
+                        Console.WriteLine("*|                                      |*");
+                        Console.WriteLine("******************************************");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        transaccion = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        switch (transaccion) 
+                        {
+                            case 1:
+                                Palmer.Pagar(100);
+                                break;
+
+                            case 2:
+                                Palmer.Pagar(500);
+                                break;
+
+                            case 3:
+                                Palmer.Pagar(200);
+                                break;
+
+                            case 4:
+                                Palmer.Pagar(1000);
+                                break;
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("esta es una opcion de administrador");
+                        Console.WriteLine("escriba la clave");
+                        string code = Console.ReadLine();
+                        Palmer.balance(code);
+                        break;
+                    case 4:
+                        sesion = false;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("gracias por utilizar nuestros servicios");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+            } while (sesion == true);
+        }
+        //CAJERO AUTOMATICO POR AXCELL ESPINAL® 9-2-2021(AxlRoseRD) made in DOMINICAN REPUBLIC
+    }
+}
+
---
+++ cajero.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+public class Cajero
+{
+    #region contenedor
+    //contenedores de billetes
+    public int Contenedor1000 = 40;   //cantidad de unidades de billetes de 1,000$
+    public int Contenedor500 = 60;   //cantidad de unidades de billetes de 500$
+    public int Contenedor200 = 100; //cantidad de unidades de billetes de 200$
+    public int Contenedor100 = 100;//cantidad de unidades de billetes de 100$  
+    #endregion
+
+     public int Pagar(int ValorTransaccion)
+    {
+        int pago = 0;
+        if (ValorTransaccion <= 10000)
+        {
+            if (ValorTransaccion >= 1000 && Contenedor1000 > 0)
+            {
+                pago = ValorTransaccion / 1000;
+                ValorTransaccion = ValorTransaccion - (pago * 1000);
+                Console.WriteLine("*se han dispensado {0} billete(s) de mil*"
+                ,pago, Console.ForegroundColor = ConsoleColor.Green);
+                Contenedor1000 -= pago;
+                Contenedor1000 = (Contenedor1000 * 1000) - (pago * 1000);
+                Console.ForegroundColor = ConsoleColor.White;
+            } 
+            if (ValorTransaccion >= 500 && Contenedor500 > 0)
+            {
+                pago = ValorTransaccion / 500;
+                ValorTransaccion = ValorTransaccion - (pago * 500);
+                Console.WriteLine("*se han dispensado {0} billete(s) de quinientos*"
+                , pago, Console.ForegroundColor = ConsoleColor.Green);
+                Contenedor500 -= pago;
+                Contenedor500 = (Contenedor500 * 500) - (pago * 500);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if (ValorTransaccion >= 200 && Contenedor200 > 0)
+            {
+                pago = ValorTransaccion / 200;
+                ValorTransaccion = ValorTransaccion - (pago * 200);
+                Console.WriteLine("*se han dispensado {0} billete(s) de docientos*"
+                , pago, Console.ForegroundColor = ConsoleColor.Green);
+                Contenedor200 -= pago;
+                Contenedor200 = (Contenedor200 * 200) - (pago * 200);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if (ValorTransaccion >= 100 && Contenedor100 > 0)
+            {
+                pago = ValorTransaccion / 100;
+                ValorTransaccion = ValorTransaccion - (pago * 100);
+                Console.WriteLine("*se han dispensado {0} billete(s) de Cien*"
+                , pago, Console.ForegroundColor = ConsoleColor.Green);
+                Contenedor100 -= pago;
+                Contenedor100 = (Contenedor100 * 100) - (pago * 100);
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+        }
+        else if (ValorTransaccion > 10000)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("monto invalido *NO PUEDE DISPENSAR CANTIDADES SUPERIORES A 10,000*");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        return pago;
+        
+    }
+    public void balance(String password)
+    {
+        if (password == "Palmer1234")
+        {
+            if (Contenedor1000 >= 30)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("quedan: {0} papeletas de MIl pesos", Contenedor1000);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Contenedor1000 > 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("quedan: {0} papeletas de MIl pesos", Contenedor1000);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Contenedor1000 <= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("quedan: {0} papeletas de MIl pesos", Contenedor1000);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        if (password == "Palmer1234")
+        {
+            if (Contenedor500 >= 50)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("quedan: {0} papeletas de quinientos pesos", Contenedor500);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Contenedor1000 >=49)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("quedan: {0} papeletas de quinientos pesos", Contenedor500);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Contenedor1000 <= 15)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("quedan: {0} papeletas de quinientos pesos", Contenedor500);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        if (password == "Palmer1234")
+        {
+            if (Contenedor200 >= 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("quedan: {0} papeletas de docientos pesos", Contenedor200);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Contenedor200 <= 30)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("quedan: {0} papeletas de docientos pesos", Contenedor200);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Contenedor200 >= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("quedan: {0} papeletas de docientos pesos", Contenedor200);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        if (password == "Palmer1234")
+        {
+            if (Contenedor100 >= 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("quedan: {0} papeletas de Cien pesos", Contenedor100);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Contenedor100 <= 30)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("quedan: {0} papeletas de Cien pesos", Contenedor100);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Contenedor100 >= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("quedan: {0} papeletas de Cien pesos", Contenedor100);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+    //CAJERO AUTOMATICO POR AXCELL ESPINAL®9-2-2021(AxlRoseRD) made in DOMINICAN REPUBLIC
+}
